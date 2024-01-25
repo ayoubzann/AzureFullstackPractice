e427d12af54ca0844e5e731bf18a3c32fbe95e52
ayoubzann <ayoubzann@gmail.com>
2024-01-25 10:44:46 +0100

    Added Models & Refactored controller

---
+++ AzureFullstackPractice/Model/Person.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureFullstackPractice.Models;
+
+public class Person
+{
+
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public int Age { get; set; }
+}
